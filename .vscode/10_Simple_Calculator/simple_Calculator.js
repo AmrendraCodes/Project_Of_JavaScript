bcee261b41062bd9c1b750665bdf7c498b96b238
amrendra1998 <techupdate855@gmail.com>
2025-06-08 23:55:16 +0530

    file commited

--- .vscode/10_Simple_Calculator/simple_Calculator.js
+++ .vscode/10_Simple_Calculator/simple_Calculator.js
@@ -15,8 +15,11 @@ function ClearDisplay() {
 
 function calculate() {
     try {
-        
         let result = eval(currentInput);
+       
+        if (typeof result === "number" && !Number.isInteger(result)) {
+            result = parseFloat(result.toFixed(10));
+        }
         display.value = result;
         currentInput = result.toString();
     } catch (e) {
@@ -26,8 +29,11 @@ function calculate() {
 }
 
 document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); 
+    }
     const key = event.key;
-     if ((key >= "0" && key <= "9") || key === "+" || key === "-" || key === "*" || key === "/" || key === ".") {
+    if ((key >= "0" && key <= "9") || key === "+" || key === "-" || key === "*" || key === "/" || key === ".") {
         Appendvalue(key);
     }
 
@@ -41,7 +47,7 @@ document.addEventListener("keydown", function(event) {
         ClearDisplay();
     }
 
-    // Optional: allow backspace
+    // Allow backspace to remove last character
     if (key === "Backspace") {
         currentInput = currentInput.slice(0, -1);
         display.value = currentInput;
